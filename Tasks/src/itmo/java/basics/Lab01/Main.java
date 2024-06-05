65b45fe7d627715e7a31471c5ff0d2e91eb356bf
Oleg Pyzin <pzn.oleg@yahoo.com>
2024-06-05 21:16:35 +0300

    Test

--- Tasks/src/itmo/java/basics/Lab01/Main.java
+++ Tasks/src/itmo/java/basics/Lab01/Main.java
@@ -2,5 +2,14 @@ package itmo.java.basics.Lab01;
 
 public class Main {
     public static void main(String[] args) {
+        String theFirst = "Я";
+        String theSecond = "хорошо";
+        String theThird = "знаю";
+        String theFourth = "Java";
+
+        System.out.println(theFirst);
+        System.out.println(theSecond);
+        System.out.println(theThird);
+        System.out.println(theFourth);
     }
 }
