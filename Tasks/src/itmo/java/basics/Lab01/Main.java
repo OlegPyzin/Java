3f81a81948ad5684e1e29fc286659ca8ed0d0602
Oleg Pyzin <pzn.oleg@yahoo.com>
2024-06-06 15:36:23 +0300

    Labaratory 01

--- Tasks/src/itmo/java/basics/Lab01/Main.java
+++ Tasks/src/itmo/java/basics/Lab01/Main.java
@@ -1,4 +1,5 @@
 package itmo.java.basics.Lab01;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +12,53 @@ public class Main {
         System.out.println(theSecond);
         System.out.println(theThird);
         System.out.println(theFourth);
+
+        double resultFirst;
+
+        resultFirst = (46 + 10) * ((double)10 / 3);
+        System.out.println(resultFirst);
+
+        int number = 10500;
+        int result;
+
+        result = (number / 10) / 10;
+        System.out.println(result);
+
+        double varFirst = 3.6;
+        double varSecond = 4.1;
+        double varThird = 5.9;
+        double resultSecond;
+
+        resultSecond = varFirst * varSecond * varThird;
+        System.out.println(resultSecond);
+
+        Scanner getHuman = new Scanner(System.in);
+        int intHumanFirst;
+        int intHumanSecond;
+        int intHumanThird;
+
+        System.out.println("Input a Number:");
+        intHumanFirst = getHuman.nextInt();
+        System.out.println("Input a Number:");
+        intHumanSecond = getHuman.nextInt();
+        System.out.println("Input a Number:");
+        intHumanThird = getHuman.nextInt();
+        System.out.println(intHumanFirst);
+        System.out.println(intHumanSecond);
+        System.out.println(intHumanThird);
+
+        int b;
+
+        System.out.println("Input a Number:");
+        b = getHuman.nextInt();
+
+        if ( b%2 == 1 ) {
+            System.out.println("Нечетное");
+        } else if ( b > 100) {
+            System.out.println("Выход за пределы диапазона.");
+        } else {
+            System.out.println("Четное");
+        }
+
     }
 }
