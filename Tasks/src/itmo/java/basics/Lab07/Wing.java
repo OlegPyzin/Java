ae099059cf143a4d3cad3ebe225281b2a7e29ae0
Oleg Pyzin <pzn.oleg@yahoo.com>
2024-06-22 10:03:28 +0300

    Lab 07.

---
+++ Tasks/src/itmo/java/basics/Lab07/Wing.java
@@ -0,0 +1,16 @@
+/* Данный класс был создан
+ * просто для изучения различных реализаций
+ */
+//package itmo.java.basics.Lab07;
+//
+//public class Wing {
+//    private double weight;
+//
+//    public void setWeight(double weight) {
+//        this.weight = weight;
+//    }
+//
+//    public double getWeight() {
+//        return this.weight;
+//    }
+//}
