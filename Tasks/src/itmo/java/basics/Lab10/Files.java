574e0d1f4ac0b41066a89ea87701d5eb2c8c4904
Oleg Pyzin <pzn.oleg@yahoo.com>
2024-06-28 14:13:14 +0300

    Lab 10.

---
+++ Tasks/src/itmo/java/basics/Lab10/Files.java
@@ -0,0 +1,114 @@
+package itmo.java.basics.Lab10;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Files {
+    public static void main(String[] args) throws Exception {
+        System.out.println("На моем компьютере используются следующие файловые системы:");
+        getFsType();
+
+        String fileName = getFileName();
+        if (fileName == null) {
+            System.out.println("Вы не ввели имени файла, поэтому ничего не делаем.");
+        } else {
+            List<String> content = readFromFile(fileName);
+            System.out.println("Читаем из файла [" + fileName + "]");
+            for (String line : content) {
+                System.out.println(line);
+            }
+        }
+
+        fileName = getFileName();
+        if (fileName == null) {
+            System.out.println("Вы не ввели имени файла, поэтому ничего не делаем.");
+        } else {
+            System.out.println("Записываем в файл [" + fileName + "]");
+            String str = "Hello world !";
+            writeToFile(fileName, str, false);
+        }
+
+        System.out.println("============================================");
+        System.out.println("п.3 Объединяем файлы");
+
+        concatFiles(getFileName(),getFileName(),getFileName());
+
+
+        System.out.println("============================================");
+        System.out.println("п.4 Заменяем символы");
+        replaceSomeLetters(getFileName(), "[^A-z0-9А-я]", "\\$");
+    }
+
+    public static void getFsType() {
+        FileSystem fsType = FileSystems.getDefault();
+        System.out.println("Примонтировано");
+        for(FileStore store: fsType.getFileStores()) {
+            System.out.println("Файловая система : [" + store.name() +"] тип [" + store.type() +"]");
+        }
+    }
+
+    public static List<String> readFromFile(String fileName) throws Exception {
+        List<String> contentFile = new ArrayList<>();
+        FileReader fr = new FileReader(fileName);
+        Scanner getFile = new Scanner(fr);
+
+        while (getFile.hasNextLine()) {
+            contentFile.add(getFile.nextLine());
+        }
+        fr.close();
+
+        return contentFile;
+    }
+
+    public static void writeToFile(String fileName, String string, boolean append) throws Exception {
+        FileWriter fw = new FileWriter(fileName, append);
+
+        fw.write(string);
+        fw.close();
+    }
+
+    public static void concatFiles(String theFirstFile, String theSecondFile, String concatFile) throws Exception {
+        List<String> tmp = readFromFile(theFirstFile);
+
+        for (String line : tmp) {
+            writeToFile(concatFile, line+"\n", true);
+        }
+
+        tmp = readFromFile(theSecondFile);
+        for (String line : tmp) {
+            writeToFile(concatFile, line+"\n", true);
+        }
+    }
+
+    public static void replaceSomeLetters(String fileName, String regex, String charToReplace) throws Exception {
+        List<String> tmp = readFromFile(fileName);
+
+        // Очищаем содержимое файла
+        writeToFile(fileName, "", false);
+
+        for (String line : tmp) {
+            writeToFile(fileName, line.replaceAll(regex, charToReplace) +"\n", true);
+        }
+    }
+
+    public static String getFileName() {
+        String fileName;
+        int len;
+        Scanner getHuman = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Введите имя файла, или Enter для завершения ввода:");
+        fileName = getHuman.nextLine();
+        fileName = fileName.trim();
+        if(fileName.length()==0) {
+            fileName = null;
+        }
+
+        return fileName;
+    }
+}
